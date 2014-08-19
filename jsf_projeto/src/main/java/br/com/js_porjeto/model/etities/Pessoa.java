a779b6f03021c41c7a6d845d2ed1d3581c4a5fa8
Br_Styl <bruno.d.dm@hotmail.com>
2014-08-19 11:42:50 -0300

    Adição da entidade Sexo.

--- jsf_projeto/src/main/java/br/com/js_porjeto/model/etities/Pessoa.java
+++ jsf_projeto/src/main/java/br/com/js_porjeto/model/etities/Pessoa.java
@@ -6,8 +6,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import org.hibernate.annotations.ForeignKey;
 //Nome da tabela no banco de dados
 
 @Entity
@@ -35,6 +37,10 @@ public class Pessoa implements Serializable{
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataDeCadastro;
 
+    @ManyToOne(optional =false)
+    @ForeignKey(name= "PessoaSexo")
+    private Pessoa pessoa;
+    
     public Pessoa() {
     }
 
